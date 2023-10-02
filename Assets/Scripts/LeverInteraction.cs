77168778486665b82985789a15728f5d23865d62
Serandt <36221849+Serandt@users.noreply.github.com>
2023-10-02 09:24:58 +0200

    Lever first step

---
+++ Assets/Scripts/LeverInteraction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverInteraction : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Collision with Lever");
+        anim.Play("leverAnimation");
+    }
+}
