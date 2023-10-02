cf8737a0fb0d7840256b4b3c254393b282801385
Delmana <35628648+Delmana@users.noreply.github.com>
2023-10-02 22:09:04 +0200

    Update player Speed

--- Assets/PlayerSpeedManager.cs
+++ Assets/PlayerSpeedManager.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerSpeedManager : MonoBehaviour
 {
     [SerializeField] private GameObject organic;
+    private float minSpeed = 3.5f;
+    private float maxSpeed = 5f;
 
     private void Update()
     {
         float f = organic.GetComponent<FloatingHealthBar>().currentHealth;
-        ChangePlayerSpeed(f/100 * 2.5f + 2.5f);
+        ChangePlayerSpeed(Mathf.Lerp(minSpeed, maxSpeed, f / 100f));
     }
 
     private void ChangePlayerSpeed(float newSpeed)
